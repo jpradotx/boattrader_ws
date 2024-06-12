b644479985c90bf763f5d8456d8eb2b491b735cf
training <pradaven9911@gmail.com>
2024-06-11 22:42:56 -0500

    update mongo script with insert entry on filter list collection

--- update_mongo_scripts.py
+++ update_mongo_scripts.py
@@ -1,16 +1,16 @@
-import time
-from datetime import datetime, date
+from datetime import datetime
 from pymongo import MongoClient
 import re
 
 MONGO_DB_NAME = "BoatTrader"
 HTML_FILE_QUEUE_COLLECTION = "file_queue"
 BOAT_DATA_COLLECTION = "boats_data"
+BOAT_FILTER_LIST_COLLECTION = "filter_url_list"
 MONGO_HOST = "localhost"
 MONGO_PORT = 27017
 
 def main_update_created_on():
-    queue_coll = initialize_mongodb()
+    queue_coll = initialize_mongodb(HTML_FILE_QUEUE_COLLECTION)
     print(queue_coll)
     find_cursor = queue_coll.find_one({"created_on": {"$ne": None}})
     while find_cursor:
@@ -28,14 +28,14 @@ def main_update_created_on():
 
 
 def main_rename_enghours():
-    working_coll = initialize_mongodb()
+    working_coll = initialize_mongodb(BOAT_DATA_COLLECTION)
     print(working_coll)
     response = working_coll.update_many({}, {"$rename": {"engineHours": "enghours_box", "EngHours_Descrip": "enghours_descrip"}})
     print(response)
 
 
 def main_create_max_eng_hrs():
-    working_coll = initialize_mongodb()
+    working_coll = initialize_mongodb(BOAT_DATA_COLLECTION)
     find_cursor = working_coll.find_one({"enghours": {"$ne": None}})
     hrs_des = find_cursor[""]
 
@@ -68,8 +68,9 @@ def initialize_mongodb(collection):
     try:
         db_client = MongoClient(host=MONGO_HOST, port=MONGO_PORT, serverSelectionTimeoutMS=2000)
         db_client.server_info()
-    except pymongo.errors.ServerSelectionTimeoutError as err:
-        return None, None
+    except Exception as err:
+        print("Error initializing mongo, no connection !!! %s", err)
+        return None
     boats_db = db_client[MONGO_DB_NAME]
     working_coll = boats_db[collection]
     return working_coll
@@ -79,7 +80,8 @@ def initialize_mongodb_both():
     try:
         db_client = MongoClient(host=MONGO_HOST, port=MONGO_PORT, serverSelectionTimeoutMS=2000)
         db_client.server_info()
-    except pymongo.errors.ServerSelectionTimeoutError as err:
+    except Exception as err:
+        print("Error initializing mongo, no connection !!! %s", err)
         return None, None
     boats_db = db_client[MONGO_DB_NAME]
     queue_coll = boats_db[HTML_FILE_QUEUE_COLLECTION]
@@ -154,14 +156,24 @@ def main_populate_enghours():
         response_missing = working_coll.find_one({"enghours": {"$exists": False}})
 
 
+def main_insert_filter_db():
+    working_coll = initialize_mongodb(BOAT_FILTER_LIST_COLLECTION)
+    print("Working collection on db: ", working_coll)
+    insert_dict = {'filter_number': '3', 'filter_name': 'test_disable',
+                   'filter_url': '/boats/test/disable/url/', 'filter_enable': 'TRUE'}
+    working_coll.insert_one(insert_dict)
+
+
+
 if __name__ == "__main__":
     current_datetime = datetime.utcnow()
     current_date = current_datetime.strftime("%Y%m%d")
-    print("Start of boatTrader webscrapper Downloader, date: ", current_datetime)
+    print("Start of boatTrader UPDATE MONGO script, date: ", current_datetime)
 
     # main_delete_entries_per_date()
     # main_change_listing_null_to_downloaded()
-    main_populate_enghours()
+    # main_populate_enghours()
+    main_insert_filter_db()
 
     current_datetime = datetime.utcnow()
-    print("END of boatTrader webscrapper Downloader, date: ", current_datetime)
+    print("END of boatTrader UPDATE MONGO script, date: ", current_datetime)
