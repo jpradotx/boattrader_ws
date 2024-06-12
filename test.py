1fde5ebb397fd8a83a269b953ab306826584f49c
training <pradaven9911@gmail.com>
2024-06-12 00:27:03 -0500

    update test script to consider filter_number to get the filter name and url from filter list collection on db

--- test.py
+++ test.py
@@ -4,11 +4,11 @@ from pathlib import Path
 import logging
 
 
-ENV_VAR_FILTER_NAME = "WSCRAP_FILTER_NAME"
-WSCRAP_FILTER_NAME = os.getenv(ENV_VAR_FILTER_NAME, "SeaRay_240_sing-out_2014_2019")
+WSCRAP_FILTER_NAME = os.getenv("WSCRAP_FILTER_NAME", "SeaRay_240_sing-out_2014_2019")
 
-ENV_VAR_FILTER_URL = "WSCRAP_FILTER_URL"
-WSCRAP_FILTER_URL = os.getenv(ENV_VAR_FILTER_URL, "/boats/make-sea-ray/engine-single+outboard/year-2014,2019/keyword-240/")
+WSCRAP_FILTER_URL = os.getenv("WSCRAP_FILTER_URL", "/boats/make-sea-ray/engine-single+outboard/year-2014,2019/keyword-240/")
+
+WSCRAP_FILTER_NUMBER = os.getenv("WSCRAP_FILTER_NUMBER", 2)
 
 ENV_VAR_MONGO_HOST = "WSCRAP_MONGO_HOST"
 MONGO_HOST = os.getenv(ENV_VAR_MONGO_HOST, "localhost")
@@ -16,12 +16,12 @@ MONGO_HOST = os.getenv(ENV_VAR_MONGO_HOST, "localhost")
 MONGO_DB_NAME = "BoatTrader"
 HTML_FILE_QUEUE_COLLECTION = "file_queue"
 BOAT_DATA_COLLECTION = "boats_data"
+BOAT_FILTER_LIST_COLLECTION = "filter_url_list"
 # MONGO_HOST = "localhost"
-# MONGO_HOST = "172.17.0.2"
 MONGO_PORT = 27017
 
 
-def initialize_mongodb():
+def initialize_mongodb(collection):
     try:
         db_client = MongoClient(host=MONGO_HOST, port=MONGO_PORT, serverSelectionTimeoutMS=2000)
         db_client.server_info()
@@ -30,8 +30,8 @@ def initialize_mongodb():
         return None
     print("DB client: %s", db_client)
     boats_db = db_client[MONGO_DB_NAME]
-    queue_coll = boats_db[HTML_FILE_QUEUE_COLLECTION]
-    return queue_coll
+    return_coll = boats_db[collection]
+    return return_coll
 
 
 def get_working_directory():
@@ -47,16 +47,17 @@ def main():
     print("Testing ENV VAR")
     print("Value of WSCRAP_FILTER_NAME: ", WSCRAP_FILTER_NAME)
     print("Value of WSCRAP_FILTER_URL: ", WSCRAP_FILTER_URL)
+    print("Value of WSCRAP_FILTER_NUMBER: ", WSCRAP_FILTER_NUMBER)
 
     # Test mongodb connection
     print("Testing mongo")
-    queue_coll = initialize_mongodb()
-    if queue_coll is None:
+    working_coll = initialize_mongodb(BOAT_FILTER_LIST_COLLECTION)
+    if working_coll is None:
         print("Not able to get queue collection object from mongo db!!")
         return
-    print("Mongo Collection: %s", queue_coll)
-    find_result = queue_coll.find_one()
-    print("mongo find_one result: ", find_result)
+    print("Mongo Collection: %s", working_coll)
+    find_result = working_coll.find_one({"filter_number": int(WSCRAP_FILTER_NUMBER)})
+    print("mongo find_one result by filter_number: ", find_result)
 
     # Test write to html files directory
     directory = get_working_directory() / Path("data_html") / Path("test_folder")
